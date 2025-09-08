f6339dab328fb327e36cc91ed960a7ec4cfa47d8
VasilevLS <lev.vas11@mail.ru>
2025-09-08 22:40:53 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,3 +1,9 @@
 f = open("input.txt").readlines()
-s = list(,ap(int, f[0]))
-print(s)
+h = open("output.txt", "w")
+s = list(map(int,f[0].split()))
+if f[1] == "+":
+    h.write(str(sum(s)))
+if f[1] == "-":
+    h.write(str(s[0]-s[1]))
+if f[1] == "*":
+    h.write(str(s[0]*s[1]))
